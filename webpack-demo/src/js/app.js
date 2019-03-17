11046c3face07d4b34933b8af594750448520d89
chenxingyu0830 <chenxingyu0830@gmail.com>
2019-03-17 18:54:32 +0800

    add webpack-demo

---
+++ webpack-demo/src/js/app.js
@@ -0,0 +1,8 @@
+import { fn1,fn2 } from './module-1'
+import y from './module-2'
+import '../css/main.scss'
+
+fn1()
+fn2()
+console.log('fuck webpack')
+y()
---
+++ webpack-demo/src/js/module-1.js
@@ -0,0 +1,6 @@
+export function fn1(){
+  console.log('这是fn1')
+}
+export function fn2(){
+  console.log('这是fn2')
+}
