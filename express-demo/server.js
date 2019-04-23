a147e3bb3fded26b79ab99d2533777d356ba5f2c
chenxingyu0830 <chenxingyu0830@gmail.com>
2019-04-23 13:59:08 +0800

    add

---
+++ express-demo/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+
+app.get('/', function (req, res) {
+  let p = path.join(__dirname, './index.html')
+  res.sendFile(p)
+});
+app.get('/frank', function (req,res){
+  res.send('hi!');
+})
+app.use('/static', express.static(__dirname + '/public'));
+
+app.listen(9999, function () {
+  console.log('Example app listening on port 9999!');
+});
